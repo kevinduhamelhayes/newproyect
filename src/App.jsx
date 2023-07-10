e57cb726506ce0a5efae0102673f46243b1d6c8f
kevinduhamelhayes <kevinduhamelh@gmail.com>
2023-07-10 20:01:07 -0300

    terminando la follow card

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,41 @@
 import "./App.css"
+import { TwitterFollowCard } from "./TwitterFollowCard.jsx"
+
+const users = [
+  {
+    userName: "midudev",
+    name: "Miguel Ángel Durán",
+    isFollowing: true,
+  },
+  {
+    userName: "pheralb",
+    name: "Pablo H.",
+    isFollowing: false,
+  },
+  {
+    userName: "PacoHdezs",
+    name: "Paco Hdez",
+    isFollowing: true,
+  },
+  {
+    userName: "TMChein",
+    name: "Tomas",
+    isFollowing: false,
+  },
+]
+
 export function App() {
   return (
-    <articlei className="tw-followCard">
-      <header className="tw-followCard-header">
-        <img
-          className="tw-followCard-avatar"
-          src="https://unavatar.io/midudev"
-          alt="el avatar de midudev"
-        />
-        <div className="tw-followCard-info">
-          <strong>miguel angel duran </strong>
-          <span className="tw-followCard-infoUserName">@midudev</span>
-        </div>
-      </header>
-      <aside>
-        <button className="tw-followCard-button">Seguir</button>
-      </aside>
-    </articlei>
+    <section className="App">
+      {users.map(({ userName, name, isFollowing }) => (
+        <TwitterFollowCard
+          key={userName}
+          userName={userName}
+          initialIsFollowing={isFollowing}
+        >
+          {name}
+        </TwitterFollowCard>
+      ))}
+    </section>
   )
 }
