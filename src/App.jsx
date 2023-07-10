03b6cbf91b4ff4eb59c23721db64d7481635a94e
kevinduhamelhayes <kevinduhamelh@gmail.com>
2023-07-10 16:52:17 -0300

    tarjeta twitter

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css"
+export function App() {
+  return (
+    <articlei className="tw-followCard">
+      <header className="tw-followCard-header">
+        <img
+          className="tw-followCard-avatar"
+          src="https://unavatar.io/midudev"
+          alt="el avatar de midudev"
+        />
+        <div className="tw-followCard-info">
+          <strong>miguel angel duran </strong>
+          <span className="tw-followCard-infoUserName">@midudev</span>
+        </div>
+      </header>
+      <aside>
+        <button className="tw-followCard-button">Seguir</button>
+      </aside>
+    </articlei>
+  )
+}
